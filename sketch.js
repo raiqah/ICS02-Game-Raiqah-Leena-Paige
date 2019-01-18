c998ad85299e00c12f2bf63d39ed40429b01a149
raiqah <42979864+raiqah@users.noreply.github.com>
2019-01-18 13:36:20 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,5 @@
 var currentScene;
 
-//SCENE 1
-
 var drawScene1 = function() {
     currentScene = 1;
     background(180, 250, 247);
@@ -14,11 +12,10 @@ var drawScene1 = function() {
     rect(141,122,150,60);
     fill(255, 255, 255);
     textSize(45);
-   text("Start", 161,165);
+    text("Start", 161,165);
+    
    
 };
-
-//SCENE 2
 
 var drawScene2 = function() {
     currentScene = 2;
@@ -37,7 +34,7 @@ textSize(30);
 text("Instructions",x,y);
 textSize(11);
 text("Avoid obsticles in each stage.",x2-5,y+35);
-text("You must do math problem if level not passed",x2-7,y2+31);
+text("If you lose, try the next harder level",x2-7,y2+31);
 text("If level completed, collect points and move on.",x2-8,y2+73);
 fill(0, 0, 0);
 rect(176,284,87,40);
@@ -47,8 +44,6 @@ text("Okay",x+53,y+203);
 fill(255, 255, 0);
 ellipse(26,43,109,109);
 };
-
-//SCENE 3
 
 var drawScene3 = function() {
     currentScene = 3;
@@ -60,14 +55,21 @@ var drawScene3 = function() {
     ellipse(20,20,100,100);
     fill(240, 240, 240);
     rect(120,92,150,200);
+    fill(3, 10, 5);
+    textSize(15);
+    text("Who's your character", 120, 131);
+    textSize(170);
+    text("?", 146, 275);
+    
+
+
+    
 };
 
-//SCENE 4
+var drawScene4 = function (){
+currentScene = 4;
 
-var drawScene4 = function(){
-   currentScene = 4;
-    
-  var redfire = function (x,y){
+var redfire = function (x,y){
  this.x = x;
  this.y=y;
  this.speed=2 ;
@@ -105,7 +107,7 @@ this.y=y;
  var avatar = function(x,y){
            this.x= x;
            this.y= y;
-           //this.img= getImage("avatars/mr-pants-orange");
+           this.img= getImage("avatars/mr-pants-orange");
            //this.lava = 0;
            
    
@@ -118,8 +120,9 @@ this.y=y;
  rect(185,174,100,100);
 background(235, 30, 30);
 fill(8, 3, 3);
-textSize(30);
-text("You touched the red!", 66, 209);
+textSize(20);
+text("You touched the red!", 99, 174);
+text("Click anywhere to try harder level", 62, 213);
 fill(250, 106, 10);
 triangle(-6, 352, -68, 405, 7, 421);
 triangle(37, 352, 2, 405, 65, 421);
@@ -152,7 +155,7 @@ avatar.prototype.down= function(){
 
      avatar.prototype.checkhit = function (fire){
 if (this.x > fire.x-10 && this.x < fire.x+10 && this.y > fire.y-10 && this.y < fire.y+100 === true) {
-  fire.speed-=1;
+  fire.speed=0;
   page();
   
 }
@@ -177,7 +180,7 @@ draw= function() {
    
 fill(84, 37, 2);
  triangle(206, 153,82, 413, 343, 421);
- 
+ noStroke();
  fill(255, 0, 0);
    ellipse(225,200,20,57);
 ellipse(207,200,20,57);
@@ -230,27 +233,23 @@ text("Score:", 5, 26);
  
     };
 
+
+
+
 };
 
-//SCENE 5
+var drawScene5 = function (){
+currentScene = 5;
 
-var drawScene5= function(){
-    
-    currentScene = 5;
-    fill(255, 0, 0);
-    ellipse(200, 200, 20, 20);
-    
-    //new scene
-
-      var redfire = function (x,y){
+ var redfire = function (x,y){
  this.x = x;
  this.y=y;
- this.speed=2 ;
+ this.speed=3 ;
  };
  
  
  redfire.prototype.move = function(){
-     this.x-=this.speed;
+    this.x-=this.speed;
      if (this.x < 0) {   
          this.x = 500;
      }
@@ -285,23 +284,26 @@ this.y=y;
            
    
 };
+
+
  var page = function (){
  
    fill(255, 0, 0);
  rect(185,174,100,100);
-background(235, 30, 30);
+background(30, 221, 235);
 fill(8, 3, 3);
-textSize(30);
-text("You touched the red!", 66, 209);
-fill(250, 106, 10);
-triangle(-6, 352, -68, 405, 7, 421);
-triangle(37, 352, 2, 405, 65, 421);
-triangle(90, 356, 58, 405, 125, 421);
-triangle(148, 356, 118, 405, 190, 421);
-triangle(206, 356, 178, 405, 249, 421);
-triangle(264, 356, 237, 405, 307, 421);
-triangle(330, 356, 299, 405, 369, 421);
-triangle(388, 356, 364, 405, 422, 421);
+textSize(20);
+text("You touched the blue!", 99, 174);
+text("Click anywhere to try next level", 79, 209);
+fill(10, 126, 250);
+ellipse(84, 370, 51, 51);
+ellipse(139, 370, 51, 51);
+ellipse(194, 370, 51, 51);
+ellipse(249, 370, 51, 51);
+ellipse(305, 370, 51, 51);
+ellipse(29, 370, 51, 51);
+ellipse(360, 370, 51, 51);
+ellipse(417, 370, 51, 51);
  
  };
 
@@ -325,11 +327,13 @@ avatar.prototype.down= function(){
 
      avatar.prototype.checkhit = function (fire){
 if (this.x > fire.x-10 && this.x < fire.x+10 && this.y > fire.y-10 && this.y < fire.y+100 === true) {
-  fire.speed-=1;
+  fire.speed=0;
   page();
   
 }
 };
+
+
 
  var high = 20;
 
@@ -344,7 +348,7 @@ if (this.x > fire.x-10 && this.x < fire.x+10 && this.y > fire.y-10 && this.y < f
  
 draw= function() {
     //new scene
-
+strokeWeight(20);
 background(0, 153, 255);
  fill(217, 204, 204);
  rect(126, 127, 171, 274);
@@ -381,17 +385,29 @@ ellipse(13, 5, 71, 76);
       mrpants.down();
     }
    mrpants.draw();
+
+
+   fill(21, 28, 28);
+    textSize(18);
+text("Score:" , 5, 26);
+
+  
+
+ 
     };
+
+
+
+
 };
 
-//SCENE 6
-
 var drawScene6 = function (){
-    currentScene= 6;
+currentScene = 6;
+
 var redfire = function (x,y){
  this.x = x;
  this.y=y;
- this.speed=3 ;
+ this.speed=4 ;
  };
  
  
@@ -439,9 +455,9 @@ this.y=y;
  rect(185,174,100,100);
 background(39, 245, 39);
 fill(8, 3, 3);
-textSize(30);
-text("You touched the green!", 44, 209);
-text("Click anywhere to try harder level", -1, 255);
+textSize(20);
+text("You touched the green!", 99, 174);
+text("Click anywhere to try harder level", 62, 213);
 fill(0, 112, 26);
 ellipse(200,356,10,90);
 ellipse(218,356,10,90);
@@ -499,7 +515,10 @@ if (this.x > fire.x-10 && this.x < fire.x+10 && this.y > fire.y-10 && this.y < f
  
 draw= function() {
     
- background(18, 124, 204);
+
+
+      
+    background(18, 124, 204);
 fill(230, 196, 110);
 rect(-2,300,603,244);
 fill(46, 217, 31);
@@ -550,18 +569,44 @@ text("Score:", 5, 26);
     };
 
 
-//SCENE 7
+
+
+
+
+background(18, 124, 204);
+fill(230, 196, 110);
+rect(-2,300,603,244);
+fill(46, 217, 31);
+ellipse(15,262,10,80);
+ellipse(27,263,10,80);
+ellipse(39,263,10,80);
+ellipse(386,262,10,80);
+ellipse(373,262,10,80);
+ellipse(361,262,10,80);
+noStroke();
+fill(255, 0, 0);
+triangle(116,175,116,217,179,202);
+ellipse(183,200,57,24);
+fill(0, 0, 0);
+ellipse(204,198,3,4);
+fill(195, 53, 230);
+triangle(268,96,216,127,212,70);
+ellipse(278,95,57,24);
+fill(0, 0, 0);
+ellipse(297,94,3,4);
+
+
 
 };
 
-var drawScene7 = function(){
+var drawScene7 = function (){
 currentScene = 7;
-};
+
 
 var redfire = function (x,y){
  this.x = x;
  this.y=y;
- this.speed=4 ;
+ this.speed=5 ;
  };
  
  
@@ -602,15 +647,16 @@ this.y=y;
    
 };
 
+
  var page = function (){
  
  fill(235, 33, 228);
  rect(185,174,100,100);
 background(245, 39, 231);
 fill(8, 3, 3);
-textSize(30);
-text("You touched the pink!", 44, 209);
-text("Click anywhere to try harder level", -1, 255);
+textSize(20);
+text("You touched the pink!", 99, 174);
+text("Click anywhere to try harder level", 62, 213);
 fill(95, 0, 110);
 ellipse(200,356,10,90);
 ellipse(218,356,10,90);
@@ -669,6 +715,8 @@ if (this.x > fire.x-10 && this.x < fire.x+10 && this.y > fire.y-10 && this.y < f
 draw= function() {
     
 
+
+      
     background(250, 225, 247);
 fill(145, 145, 145);
 rect(150,70,100,314);
@@ -693,6 +741,8 @@ ellipse(199,135,47,62);
         mrpants.checkhit(lava[i]);
     }
     
+
+    
     
       if (keyIsPressed){
       mrpants.up();
@@ -709,17 +759,16 @@ text("Score:", 5, 26);
 
  
     };
-
-//SCENE 8
-
-var drawScene8 = function() {
-    currentScene = 8;
 };
+
+
+var drawScene8 = function (){
+currentScene = 8;
 
 var redfire = function (x,y){
  this.x = x;
  this.y=y;
- this.speed=5 ;
+ this.speed=6 ;
  };
  
  
@@ -740,6 +789,7 @@ var redfire = function (x,y){
  
 
  
+ 
  //object of lava
  var lava = [];
  
@@ -759,16 +809,16 @@ this.y=y;
    
 };
 
+
  var page = function (){
  
    fill(255, 0, 0);
  rect(185,174,100,100);
 background(55, 76, 77);
 fill(8, 3, 3);
-textSize(39);
-text("You touched the black!", 17, 177);
 textSize(20);
-text("Click anywhere to try harder level", 79, 209);
+text("You touched the black!", 99, 174);
+text("Click anywhere to try harder level", 62, 213);
 fill(207, 216, 227);
 ellipse(84, 370, 51, 51);
 ellipse(139, 370, 51, 51);
@@ -815,6 +865,7 @@ if (this.x > fire.x-10 && this.x < fire.x+10 && this.y > fire.y-10 && this.y < f
 
     }
 
+
 //DRAW FUNCTION
  
 draw= function() {
@@ -850,10 +901,19 @@ ellipse(380, 234, 83, 69);
       mrpants.down();
     }
    mrpants.draw();
+
+};
+   /*fill(21, 28, 28);
+    textSize(18);
+text("Score:", 5, 26);*/
+
+  
+
 };
 
-//SCENE 9
-var drawScene9 = function(){};
+
+var drawScene9 = function (){
+currentScene = 9;
 
 var redfire = function (x,y){
  this.x = x;
@@ -906,9 +966,9 @@ this.y=y;
  rect(185,174,100,100);
 background(227, 224, 227);
 fill(8, 3, 3);
-textSize(30);
-text("You touched the grey",44,209);
-text("Click anywhere to try harder level", -1, 255);
+textSize(20);
+text("You touched the grey",99,174);
+text("Click anywhere to try harder level", 62, 213);
 fill(0, 0, 0);
 ellipse(200,356,10,90);
 ellipse(218,356,10,90);
@@ -1008,7 +1068,13 @@ rect(174,45,130,61);
     textSize(18);
 text("Score:", 5, 26);
   
+
+ 
     };
+
+
+
+};
 
 var x=0;
 var y=0;
@@ -1026,17 +1092,18 @@ mouseClicked = function() {
         drawScene6();
     }else if (currentScene === 6){
         drawScene7();
+        
     }else if (currentScene === 7){
         drawScene8();
-    }else if (currentScene ===8){
-       drawScene9();
-    } else if (currentScene === 9){
+        
+    }else if (currentScene === 8) {
+        drawScene9();
+    
+    }else if (currentScene === 9) {
+        
         drawScene1();
+        
     }
-    };
-     
-       
-drawScene1();
-//drawScene2();
-//drawScene3(); 
+};
 
+drawScene1();
